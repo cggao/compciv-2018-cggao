0d334ec8fce2f774476c72ae8dc2e789d6089ec9
Caroline Gao <34894152+cggao@users.noreply.github.com>
2018-02-06 15:39:28 -0800

    Update scraper.py

--- week-03/stanford_headlinez/scraper.py
+++ week-03/stanford_headlinez/scraper.py
@@ -1,35 +1,30 @@
 def print_hedz(url='https://www.stanford.edu/news/'):
+    
     txt = fetch_html(url)
     htags = parse_headline_tags(txt)
+    
     for t in htags:
         hedtxt = extract_headline_text(t)
         print(hedtxt)
 
-
 def extract_headline_text(txt):
-    iterator = 0
-    headline = ""
-    for chr in txt:
-        if iterator == 2:
-            if chr == '<':
-                iterator += 1
-            else:
-                headline += chr
-        if chr == '>':
-            iterator += 1
-    return headline
-
+    headline = txt.split('<')
+    for x in headline:
+        if 'href' in x:
+            return x.split('>')[1]
 
 def parse_headline_tags(txt):
+    
+    htmlstrings = []
     lines = txt.splitlines()
-    list = []
-    for line in lines:
-        if "<h3><a" in line:
-            list.append(line)
-    return list
-
+    for x in lines:
+        if 'h3><a' in x:
+            htmlstrings.append(x)
+    return htmlstrings
 
 def fetch_html(url):
+    
     import requests
-    html = requests.get(url)
-    return html.text
+    resp = requests.get(url)
+    return resp.text
+
